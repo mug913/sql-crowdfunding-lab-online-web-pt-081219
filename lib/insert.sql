27a4d5de16b4d452df10031551d3845b2a5eed02
Brad Alexander <brad.alexander913@gmail.com>
2019-10-20 20:06:09 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,36 +1,18 @@
-INSERT INTO projects VALUES
-(1, "Proj_one", "cat1" ,100, "May 1", "Jan 5");
-(2, "Proj_two", "cat2" ,200, "May 2", "Jan 6");
-(3, "Proj_three", "cat3" ,300, "May 3", "Jan 7");
-(4, "Proj_four", "cat4" ,400, "May 4",  "Jan 8");
-(5, "Proj_five","cat2" ,  500, "May 5", "Jan 9");
-(6, "Proj_six","cat3" ,  600, "May 6", "Jan 10");
-(7, "Proj_seven","cat4" , 700,"May 7", "Jan 11");
-(8, "Proj_eight","cat1" , 800, "May 8", "Jan 12");
-(9, "Proj_nine","cat2" , 900, "May 9", "Jan 13");
-(10, "Proj_ten","cat1" , 1000, "May 10", "Jan 14");
+INSERT INTO projects VALUES (1, 'Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30');
+INSERT INTO projects VALUES (2, 'My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01');
+INSERT INTO projects VALUES (3, 'The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30');
+INSERT INTO projects VALUES (4, 'Animal shelter needs dog food', 'charity', 400.00, '2013-03-14', '2013-06-30');
+INSERT INTO projects VALUES (5, 'Voldement needs a body', 'charity', 6000.00, '2013-03-20', '2013-09-20');
+INSERT INTO projects VALUES (6, 'The next Inna-Gadda-Davida', 'music', 200.00, '2014-12-30', '2015-12-30');
+INSERT INTO projects VALUES (7, 'Iguana needs tail operation', 'charity', 2000.00, '2013-10-02', '2013-10-30');
+INSERT INTO projects VALUES (8, 'I have bed bugs!', 'charity', 800.00, '2014-06-30', '2014-06-31');
+INSERT INTO projects VALUES (9, 'I want to teach English in China', 'charity', 3000.00, '2013-06-30', '2013-09-30');
+INSERT INTO projects VALUES (10, 'Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
 
-INSERT INTO users VALUES
-(1, "a", 13);
-(2,"b", 12);
-(3,"c", 13);
-(4,"d", 14);
-(5"e", 15);
-(6,"f", 16);
-(7,"h", 17);
-(8,"g", 18);
-(9,"i", 19);
-(10,"j", 11);
-(11,"k", 12);
-(12,"l", 13);
-(13,"m", 14);
-(14,"n", 15);
-(15,"o", 16);
-(16,"p", 17);
-(17,"q", 18);
-(18,"r", 19);
-(19,"s", 11);
-(20,"t", 12);
+INSERT INTO users (id, name, age) VALUES (1, 'Finnebar', 17), (2, 'Bear', 6), (3, 'Iguana', 4), (4, 'Alex', 33),
+(5, 'Amanda', 24), (6, 'Sophie', 24), (7, 'Rosey', 9), (8, 'Victoria', 23), (9, 'Franz', 100), (10, 'Hermione', 30),
+(11, 'Voldemort', 90), (12, 'Marisa', 24), (13, 'Swizzle', 4), (14, 'Sirius', 36), (15, 'Albus', 113), (16, 'Squid', 5),
+(17, 'Whale', 6), (18, 'Pacha', 5), (19, 'Ena', 24), (20, 'Katie', 24);
 
 INSERT INTO pledges VALUES
 (1, 10.00, 1, 2),
